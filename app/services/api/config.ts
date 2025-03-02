3651ffb24b30c2086bbfc92531db258317a18da2
Fellipe Marcel Maia Silva <fellipe.marcel@cloudwalk.io>
2025-03-02 06:37:48 -0300

    feat: add people endpoint to API configuration

--- app/services/api/config.ts
+++ app/services/api/config.ts
@@ -2,6 +2,7 @@ export const API_CONFIG = {
   BASE_URL: 'https://swapi.dev/api',
   ENDPOINTS: {
     PLANETS: '/planets',
+    PEOPLE: '/people',
   },
   DEFAULT_HEADERS: {
     'Content-Type': 'application/json',
