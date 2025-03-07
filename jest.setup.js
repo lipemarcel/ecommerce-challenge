d3de6510b9c33364d9a20d5fe91452d8bf6b70c2
Fellipe Marcel Maia Silva <fellipe.marcel@cloudwalk.io>
2025-03-07 09:29:09 -0300

    test: configure global mocks and test cleanup for Jest

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom'
+import { TextEncoder, TextDecoder } from 'util'
+
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
+
+global.fetch = jest.fn()
+
+// Limpa todos os mocks após cada teste
+afterEach(() => {
+  jest.clearAllMocks()
+})
